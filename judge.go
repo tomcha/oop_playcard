07bbdf2dd976c008064f7e09fb36c3cbd36a5453
tomcha <tomcha@tomcha.net>
2022-08-16 08:10:35 +0900

    fix

--- judge.go
+++ judge.go
@@ -2,78 +2,80 @@ package main
 
 import "fmt"
 
-var winner *Player
+type Judge struct {
+	name string
+}
 
-func startJanken(p1 *Player, p2 *Player) {
+func (j *Judge) startJanken(p1 *Player, p2 *Player) {
 	fmt.Println("じゃんけん開始")
 
 	for i := 0; i < 3; i++ {
 		fmt.Printf("%d回戦目\n", i+1)
 
-		winner = judgeJanken(p1, p2)
+		winner := j.judgeJanken(p1, p2)
 
 		if winner != nil {
-			fmt.Printf("%s が勝ちました\n", winner.getName)
+			fmt.Printf("%s が勝ちました\n", winner.getName())
 			winner.notifyResult(true)
 		} else {
 			fmt.Println("引き分けです")
 		}
 	}
 	fmt.Println("終了")
-	finalWinner := judgeFinalWinner(p1, p2)
-	fmt.Plintf("%d 対 %d で", p1.getWinCount, p2.getWinCount)
+	finalWinner := j.judgeFinalWinner(p1, p2)
+	fmt.Printf("%d 対 %d で", p1.getWinCount(), p2.getWinCount())
 	if finalWinner != nil {
-		fmt.Printf("%s の勝ちです\n", finalWinner.getName)
+		fmt.Printf("%s の勝ちです\n", finalWinner.getName())
 	} else {
 		fmt.Println("引き分けです")
 	}
 }
 
-func judgeJanken(p1 *Player, p2 *Player) *Player {
-	winner = nil
+func (j *Judge) judgeJanken(p1 *Player, p2 *Player) *Player {
+	var winner *Player
 	var p1Hand int
 	var p2Hand int
 
-	p1Hand = p1.showHand
-	p2Hand = p2.showHand
+	p1Hand = p1.showHand()
+	p2Hand = p2.showHand()
 
-	printHand(p1Hand)
+	j.printHand(p1Hand)
 	fmt.Printf(" vs. ")
-	printHand(p2Hand)
+	j.printHand(p2Hand)
 	fmt.Print("\n")
 
-	if (p1Hand == Player.stone && p2Hand == Player.scissor) ||
-		(p1Hand == Player.scissor && p2Hand == Player.paper) ||
-		(p1Hand == Player.paper && p2Hand == Player.stone) {
+	if (p1Hand == stone && p2Hand == scissor) ||
+		(p1Hand == scissor && p2Hand == paper) ||
+		(p1Hand == paper && p2Hand == stone) {
 		winner = p1
-	} else if (p1Hand == Player.stone && p2Hand == Player.paper) ||
-		(p1Hand == Player.scissor && p2Hand == Player.stone) ||
-		(p1Hand == Player.pappper && p2Hand == Player.stone) {
+	} else if (p1Hand == stone && p2Hand == paper) ||
+		(p1Hand == scissor && p2Hand == stone) ||
+		(p1Hand == paper && p2Hand == stone) {
 		winner = p2
 	}
 	return winner
 }
 
-func judgeFinalWinner(p1 *Player p2 *Player) {
-    winner := nil
-	p1WinCount := p1.getWinCouont()
+func (j *Judge) judgeFinalWinner(p1 *Player, p2 *Player) *Player {
+	var winner *Player
+	p1WinCount := p1.getWinCount()
 	p2WinCount := p2.getWinCount()
 
-	if p1WinCount > p2WinCount{
+	if p1WinCount > p2WinCount {
 		winner = p1
-	} else if p1.WinCount < p2WinCount{
+	} else if p1.WinCount < p2WinCount {
 		winner = p2
 	}
 	return winner
 }
 
-func printHad(int hand) {
-	switch hand{
-	case Player.stone:
+func (j *Judge) printHand(hand int) {
+	switch hand {
+	case stone:
 		fmt.Print("グー")
-	case Player.scissor:
+	case scissor:
 		fmt.Print("チョキ")
-	case Player.paper:
+	case paper:
 		fmt.Print("パー")
 	}
 }
