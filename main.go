2311bf0acd9ea11059eece2ac1cb3b803c2e6fb0
tomcha <tomcha@tomcha.net>
2022-08-23 08:06:54 +0900

    embedを使った形式にmainを書き換え

--- main.go
+++ main.go
@@ -8,15 +8,8 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	saito := Judge{"斎藤さん"}
-	murata := Player{"村田さん", 0}
-	yamada := Player{"山田さん", 0}
-	/*	var murata Murata
-		var yamada Yamada
-		murata.Player.Name = "村田さん"
-		murata.Player.WinCount = 0
+	murata := Murata{Player: Player{"村田さん", 0}}
+	yamada := Yamada{Player: Player{"村田さん", 0}}
 
-		yamada.Player.Name = "山田さん"
-		yamada.Player.WinCount = 0
-	*/
 	saito.startJanken(&murata, &yamada)
 }
