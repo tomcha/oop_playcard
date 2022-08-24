301d43ae659db8c5201bbdcfbb810c8d2fba81f1
tomcha <tomcha@tomcha.net>
2022-08-25 08:25:23 +0900

    showHand の固定メソッドを削除

--- murata.go
+++ murata.go
@@ -3,7 +3,3 @@ package main
 type Murata struct {
 	Player
 }
-
-func (m *Murata) showHand() int {
-	return stone
-}
--- yamada.go
+++ yamada.go
@@ -3,7 +3,3 @@ package main
 type Yamada struct {
 	Player
 }
-
-func (y *Yamada) showHand() int {
-	return paper
-}
