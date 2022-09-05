147de5b2737ece5ae8a3ff95f1f0b06c496f5e41
tomcha <tomcha@tomcha.net>
2022-09-06 08:24:52 +0900

    fmt.Scanのタイミングを修正

--- ask_tactics.go
+++ ask_tactics.go
@@ -7,12 +7,12 @@ type askTactics struct{}
 func (t askTactics) readTactics() int {
 	var s string
 	for {
-		fmt.Scan(&s)
 		fmt.Println("何を出しますか？")
 		fmt.Println("1: グー")
 		fmt.Println("2: チョキ")
 		fmt.Println("3: パー")
 		fmt.Print("input number >>>")
+		fmt.Scan(&s)
 		if s == "1" {
 			fmt.Println("グーを選びました")
 			return stone
