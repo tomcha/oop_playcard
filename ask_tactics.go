06eac2ec8a297189242615abfa2efd9060fe462f
tomcha <tomcha@tomcha.net>
2022-09-06 08:22:57 +0900

    メッセージ出力のタイミングを修正

--- ask_tactics.go
+++ ask_tactics.go
@@ -5,14 +5,14 @@ import "fmt"
 type askTactics struct{}
 
 func (t askTactics) readTactics() int {
-	fmt.Println("何を出しますか？")
-	fmt.Println("1: グー")
-	fmt.Println("2: チョキ")
-	fmt.Println("3: パー")
-	fmt.Print("input number >>>")
 	var s string
 	for {
 		fmt.Scan(&s)
+		fmt.Println("何を出しますか？")
+		fmt.Println("1: グー")
+		fmt.Println("2: チョキ")
+		fmt.Println("3: パー")
+		fmt.Print("input number >>>")
 		if s == "1" {
 			fmt.Println("グーを選びました")
 			return stone
